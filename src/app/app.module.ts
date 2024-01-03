1c35fed8426767cfaacafebaea47578da9e8427d
Mélanie Manivanh <melanie.manivanh@gmail.com>
2024-01-03 15:36:28 +0100

    git add timer and point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PointsComponent } from './points/points.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PointsComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
