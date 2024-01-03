b3ac3e39083ad2aca484da5e47eb38990087f4a4
m3lies <melanie.manivanh@gmail.com>
2024-01-03 17:33:58 +0100

    routed view and added variable points.component.html

--- src/app/points/points.component.ts
+++ src/app/points/points.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-points',
@@ -12,4 +12,5 @@ export class PointsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Input() point?: number;
 }
